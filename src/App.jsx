d4b5aa6e73810a105958933d33514cade9a08f62
sagarYadav <yadavfamilyinfo03@gmail.com>
2025-12-24 18:15:08 +0545

    bg button design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Button from "./components/Button";
+import React from 'react'
+import Button from './components/Button'
 
 const App = () => {
   return (
-    <>
+    <div>
       <Button />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ changeBg }) => {
   return (
-<>
-    <div>Button</div>
-    <div>Button2</div>
-    <div>Button3</div>
-    <div>Button4</div>
-    <div>Button5</div>
-    <div>Button5</div>
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 
+                    bg-white shadow-lg rounded-xl px-4 py-3 
+                    flex gap-3">
+      
+      <button
+        onClick={() => changeBg("red")}
+        className="px-4 py-2 rounded-lg bg-red-500 text-white hover:scale-105 transition"
+      >
+        Red
+      </button>
 
-</>
-)
-}
+      <button
+        onClick={() => changeBg("green")}
+        className="px-4 py-2 rounded-lg bg-green-500 text-white hover:scale-105 transition"
+      >
+        Green
+      </button>
 
-export default Button
+      <button
+        onClick={() => changeBg("blue")}
+        className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:scale-105 transition"
+      >
+        Blue
+      </button>
+
+      <button
+        onClick={() => changeBg("yellow")}
+        className="px-4 py-2 rounded-lg bg-yellow-400 text-black hover:scale-105 transition"
+      >
+        Yellow
+      </button>
+
+      <button
+        onClick={() => changeBg("black")}
+        className="px-4 py-2 rounded-lg bg-black text-white hover:scale-105 transition"
+      >
+        Dark
+      </button>
+
+    </div>
+  );
+};
+
+export default Button;
